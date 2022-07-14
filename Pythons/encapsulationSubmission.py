d31469bdb22d06e99d1d9cb8a097aac7ca615927
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-07-14 16:35:41 -0700

    Add files via upload

--- Pythons/encapsulationSubmission.py
+++ Pythons/encapsulationSubmission.py
@@ -1,20 +1,42 @@
 
-#Creating class
-class Protected:
+#Creating Private class
+class Private:
     def __init__(self):
-        self._privateVar = 0 #_private denotes protected var. no touchy!
+        self.__privateVar = 0 #_private denotes protected 
 
     def getPrivate(self):
-        print(self._privateVar)
+        print(self.__privateVar)
 
     def setPrivate(self, private):
-        self._privateVar = private
+        self.__privateVar = private
 
 # Object gets the data of the private variable
 # the variable was also set to a new value of 5 
 
-obj = Protected()
+obj = Private()
 obj.getPrivate()
 obj.setPrivate(5)
 obj.getPrivate()
-    
+
+
+#Creating Protected class    
+class Protected:
+    def __init__(self):
+        self._protectedVar = 1
+
+    def getProtected(self):
+        print(self._protectedVar)
+
+    def setProtected(self, protected):
+        self._protectedVar = protected
+
+# Object gets the data of the protected variable
+# the variable was also set to a new value of 6 
+
+obj = Protected()
+obj.getProtected()
+obj.setProtected(6)
+obj.getProtected()
+
+
+
